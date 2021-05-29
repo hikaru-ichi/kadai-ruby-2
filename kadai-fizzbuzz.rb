5c1237919dd7b9ab56a174aef9110223a3970a9a
hikaru <thieryhikarin0851@gmail.com>
2021-05-29 05:20:40 +0000

    first commit

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,17 @@
+def FizzBuzz(num)
+  if (num %3 == 0) && (num %5 == 0)
+      return "FizzBuzz"
+  elsif num %3 == 0
+      return "Fizz"
+  elsif num %5 == 0
+      return "Buzz"
+  else 
+      return num
+  end
+end
+
+num_max = 100
+(1 .. num_max).each do |num|
+  return_num = FizzBuzz(num)
+  puts return_num
+end
